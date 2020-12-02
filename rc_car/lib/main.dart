cdcd4a6878d83437e90f8bdfe3904239b9ec433b
b21627428 <b21627428@cs.hacettepe.edu.tr>
2020-12-02 14:28:48 +0300

    done

---
+++ rc_car/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => HomePage(),
+      },
+    ),
+  );
+}
+
+
